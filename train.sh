f7fc6f6f61a60c86b6ab8b06b08d150cbcd684cd
zhilin wang <wangzhilin12061996@hotmail.com>
2021-05-15 21:35:01 -0700

    correct typo

--- train.sh
+++ train.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 if [ $2 == 'inference' ]
 then
-  python3 model/generator_model.py --load_trained_model False --debug_mode False --config_name $1 --lr 2.5e-3 --mode head_reln_tail --data_subset not_within_sentence
-  python3 model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset not_within_sentence --inference_only True --generation_name tail_spans_associated_with_relation+all_ten
-  python3 model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset not_within_sentence --inference_only True --generation_name tail_spans_associated_with_relation+all_ten --generate_train True
-  python3 model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset not_within_sentence --inference_only True --generation_name tail_spans_associated_with_relation+all_ten --generate_test True
-  python3 model/reranker_model.py --load_trained_model False --debug_mode False --config_name $1 --lr 5e-6 --mode head_reln_tail --epochs 8 --inference_only False --model_name bert --train_dataset_filename train_tokens_epoch_tail_spans_associated_with_relation+all_ten.csv
-  python3 model/reranker_model.py --load_trained_model True --debug_mode False --config_name $1 --lr 5e-6 --mode head_reln_tail --epochs 8 --inference_only True --model_name bert --train_dataset_filename train_tokens_epoch_tail_spans_associated_with_relation+all_ten.csv --generate_test True
+  python model/generator_model.py --load_trained_model False --debug_mode False --config_name $1 --lr 2.5e-3 --mode head_reln_tail --data_subset not_within_sentence
+  python model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset not_within_sentence --inference_only True --generation_name tail_spans_associated_with_relation+all_ten
+  python model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset not_within_sentence --inference_only True --generation_name tail_spans_associated_with_relation+all_ten --generate_train True
+  python model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset not_within_sentence --inference_only True --generation_name tail_spans_associated_with_relation+all_ten --generate_test True
+  python model/reranker_model.py --load_trained_model False --debug_mode False --config_name $1 --lr 5e-6 --mode head_reln_tail --epochs 8 --inference_only False --model_name bert --train_dataset_filename train_tokens_epoch_tail_spans_associated_with_relation+all_ten.csv
+  python model/reranker_model.py --load_trained_model True --debug_mode False --config_name $1 --lr 5e-6 --mode head_reln_tail --epochs 8 --inference_only True --model_name bert --train_dataset_filename train_tokens_epoch_tail_spans_associated_with_relation+all_ten.csv --generate_test True
 else
-  python3 model/generator_model.py --load_trained_model False --debug_mode False --config_name $1 --lr 7.5e-4 --mode head_reln_tail --data_subset within_sentence
-  python3 model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset within_sentence --inference_only True --generation_name original_spans+all_ten
-  python3 model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset within_sentence --inference_only True --generation_name original_spans+all_ten --generate_train True
-  python3 model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset within_sentence --inference_only True --generation_name original_spans+all_ten --generate_test True
-  python3 model/reranker_model.py --load_trained_model False --debug_mode False --config_name $1 --lr 5e-6 --mode head_reln_tail --epochs 8 --inference_only False --model_name bert --train_dataset_filename train_tokens_epoch_original_spans+all_ten.csv
-  python3 model/reranker_model.py --load_trained_model True --debug_mode False --config_name $1 --lr 5e-6 --mode head_reln_tail --epochs 8 --inference_only True --model_name bert --train_dataset_filename train_tokens_epoch_original_spans+all_ten.csv --generate_test True
+  python model/generator_model.py --load_trained_model False --debug_mode False --config_name $1 --lr 7.5e-4 --mode head_reln_tail --data_subset within_sentence
+  python model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset within_sentence --inference_only True --generation_name original_spans+all_ten
+  python model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset within_sentence --inference_only True --generation_name original_spans+all_ten --generate_train True
+  python model/generator_model.py --load_trained_model True --debug_mode False --config_name $1 --mode head_reln_tail --data_subset within_sentence --inference_only True --generation_name original_spans+all_ten --generate_test True
+  python model/reranker_model.py --load_trained_model False --debug_mode False --config_name $1 --lr 5e-6 --mode head_reln_tail --epochs 8 --inference_only False --model_name bert --train_dataset_filename train_tokens_epoch_original_spans+all_ten.csv
+  python model/reranker_model.py --load_trained_model True --debug_mode False --config_name $1 --lr 5e-6 --mode head_reln_tail --epochs 8 --inference_only True --model_name bert --train_dataset_filename train_tokens_epoch_original_spans+all_ten.csv --generate_test True
